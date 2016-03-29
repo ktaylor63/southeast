296883cb908d2afbbdebc7766cc7e667037ab126
Roy Hewitt <roy_hewitt@fws.gov>
2016-03-29 15:15:21 -0400

    Blow away dist before regenerating

--- build/hugo.js
+++ build/hugo.js
@@ -1,20 +1,26 @@
 (function () {
   'use strict';
 
-  var spawn = require('child_process').spawn;
-  var hugo = spawn('hugo', [
-    '--config=site/config.yml',
-    '--destination=../dist/',
-    '--source=site/',
-    '--baseURL=' + process.argv[3]
-  ]);
+  var rimraf = require('rimraf');
 
-  hugo.stdout.on('data', function (data) {
-    console.log(data.toString('utf8'));
+  rimraf('dist', function (err) {
+    if (err) console.error(err);
+    
+    var spawn = require('child_process').spawn;
+    var hugo = spawn('hugo', [
+      '--config=site/config.yml',
+      '--destination=../dist/',
+      '--source=site/',
+      '--baseURL=' + process.argv[3]
+    ]);
+
+    hugo.stdout.on('data', function (data) {
+      console.log(data.toString('utf8'));
+    });
+
+    hugo.stderr.on('data', function (data) {
+      console.error(data.toString('utf8'));
+    });
+
   });
-
-  hugo.stderr.on('data', function (data) {
-    console.error(data.toString('utf8'));
-  });
-
 })();
