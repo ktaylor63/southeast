fb8d0b8206460351a94a5f3a7787081cf6e9e3f0
Roy Hewitt <roy_hewitt@fws.gov>
2016-05-11 16:18:07 -0400

    Include scrollnav on every page

--- src/js/every-page/index.js
+++ src/js/every-page/index.js
@@ -4,8 +4,13 @@
   var fs = require('fs');
 
   var menu = require('fws-navigation/src/js/menu');
+  var nav = require('fws-scrollnav');
   var glossary = require('fws-glossary');
   var highlighter = require('fws-highlighter');
+  var hasClass = require('has-class');
+  var addClass = require('add-class');
+  var removeClass = require('remove-class');
+
   var terms = fs.readFileSync(__dirname + '/terms.json', 'utf8');
   terms = JSON.parse(terms);
 
@@ -16,6 +21,13 @@
     this.field('acronym', { boost: 3 });
     this.ref('id');
   };
+
+  nav.init({
+    content: document.getElementById('content'),
+    insertTarget: document.querySelector('.side-nav'),
+    showHeadline: false,
+    scrollOffset: 55
+  });
 
   highlighter.init({
     words: terms.map(function (term) {
@@ -38,5 +50,12 @@
   document.querySelector('.glossary-trigger').addEventListener('click', glossary.toggle);
   document.querySelector('.fws-menu-trigger').addEventListener('click', menu.show);
   document.getElementById('search-trigger').addEventListener('click', menu.toggleSearch);
+  var scrollNav = document.querySelector('.scroll-nav');
+  scrollNav.addEventListener('click', toggleScrollNav);
+
+  function toggleScrollNav() {
+    if ( hasClass(scrollNav, 'open') ) removeClass(scrollNav, 'open');
+    else addClass(scrollNav, 'open');
+  }
 
 })();
