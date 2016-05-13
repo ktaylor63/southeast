fe133b09c1ac1e5b3f6350239f77b4808a744ea6
Roy Hewitt <roy_hewitt@fws.gov>
2016-05-13 10:06:32 -0400

    Add build script to minify json

---
+++ build/minify-json.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  var jsonminify = require('jsonminify');
+  var glob = require('glob');
+  var fs = require('fs');
+  var path = require('path');
+
+  var jsonSrc = 'src/data/**/*.json';
+  var dist = 'site/static/data/';
+
+  glob(jsonSrc, function (err, files) {
+    if (err) console.error(err);
+
+    files.forEach(function (file) {
+      var basename = path.basename(file);
+      var compressed = jsonminify( fs.readFileSync(file, 'utf8') );
+      fs.writeFileSync(dist + basename, compressed);
+    });
+  });
+
+})();
