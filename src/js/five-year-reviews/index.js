1ca5146cf4c6689d80cce7791e794e1adfe9e4d5
Roy Hewitt <roy_hewitt@fws.gov>
2016-08-10 15:36:10 -0400

    Filter list of documents on input keyup

--- src/js/five-year-reviews/index.js
+++ src/js/five-year-reviews/index.js
@@ -3,11 +3,35 @@
 
   var fs = require('fs');
   var moment = require('moment');
+  var filter = require('lodash.filter');
+
   var template = require('./li.jade');
 
   var list = document.querySelector('.five-year-review-list');
+  var input = document.querySelector('.five-year-review-search');
   var species = JSON.parse( fs.readFileSync('./documents.js') );
 
-  list.innerHTML = template({ species: species });
+  render(species);
+
+  input.addEventListener('keyup', search);
+
+  function search(e) {
+    var query = e.target.value.toLowerCase(),
+        filtered;
+
+    if (query.length === 0) render(species);
+
+    filtered = filter(species, function (animal) {
+      var isName = animal.commonName.toLowerCase().indexOf(query) > -1;
+      var isStatus = animal.status.toLowerCase().indexOf(query) > -1;
+      var isTaxon = animal.taxon.toLowerCase().indexOf(query) > -1;
+      return (isName || isStatus || isTaxon);
+    });
+    render(filtered);
+  }
+
+  function render(species) {
+    list.innerHTML = template({ species: species });
+  }
 
 })();
