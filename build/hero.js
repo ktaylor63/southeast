6a46b1b01e36e83fe48c1de3fed6a9f9d0bd4461
Roy Hewitt <roy_hewitt@fws.gov>
2016-09-26 13:43:07 -0400

    Update hero image script to include a watcher by default

--- build/hero.js
+++ build/hero.js
@@ -4,6 +4,7 @@
   var imagemin = require('imagemin');
   var imageminMozjpeg = require('imagemin-mozjpeg');
   var rimraf = require('rimraf');
+  var chokidar = require('chokidar');
 
   var fs = require('fs');
   var path = require('path');
@@ -17,22 +18,45 @@
   var i = images.indexOf('.DS_Store');
   if (i > -1) images.splice(i,1);
 
-  rimraf(output + '*', function(err) {
-    if (err) console.error(err);
-    processHeroImages(images);
-  });
+  // 1. Clean the output directory site/static/images/hero/
+  // 2. Loop through all hero images in src/images/hero
+  //    3. Resize image to 1400px wide
+  //    4. Optimize image
+  //    5. Write file to output directory site/static/images/hero/
+  // 6. If the PRODUCTION env variable is NOT set kick off watcher
 
-  function processHeroImages(images) {
-    images.forEach(function (name) {
-      var img = sharp(input + name);
-      img
-        .resize(outSize)
-        .toBuffer(function (err, buffer, info) {
-          if (err) console.error(err);
-          var filename = output + name;
-          minify(buffer, filename);
-        });
-    });
+  rimraf(output + '*', init);
+
+  function init() {
+    if (process.env.PRODUCTION) {
+      console.log('Processing hero images...');
+      images.forEach(function(name) {
+        processHeroImage( path.join(input, name) );
+      });
+    } else {
+      console.log('Processing hero images and watching for changes...');
+      watcher();
+    }
+  }
+
+  function processHeroImage(filepath) {
+    var img = sharp(filepath);
+    var filename = path.basename(filepath);
+    img
+      .resize(outSize)
+      .toBuffer(function (err, buffer, info) {
+        if (err) console.error(err);
+        var outfile = output + filename;
+        minify(buffer, outfile);
+      });
+  }
+
+  function watcher() {
+    var glob = input + '*';
+    chokidar.watch(glob)
+      .on('add', processHeroImage)
+      .on('change', processHeroImage)
+      .on('unlink', removeImage);
   }
 
   function minify(buffer, filename) {
@@ -44,6 +68,18 @@
       fs.writeFile(filename, buffer, 'utf8', function(err) {
         if (err) console.error(err);
       });
+    });
+  }
+
+  function removeImage(filepath) {
+    var fileToDelete = path.join( output + path.basename(filepath) );
+    fs.access(fileToDelete, function(err) {
+      if (err) console.error(err);
+      else {
+        fs.unlink(fileToDelete, function(err) {
+          if (err) console.error(err);
+        });
+      }
     });
   }
 
