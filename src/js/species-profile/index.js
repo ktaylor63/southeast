789bd8c215496a86399ff92ebd506519f3b65de5
Roy Hewitt <roy_hewitt@fws.gov>
2016-09-28 10:40:26 -0400

    Remove logging statement

--- src/js/species-profile/index.js
+++ src/js/species-profile/index.js
@@ -14,8 +14,10 @@
   var url = 'https://www.federalregister.gov/api/v1/articles.json?per_page=1000&order=relevance&conditions%5Bterm%5D=' + animal + '&conditions%5Bagencies%5D%5B%5D=fish-and-wildlife-service';
 
   jsonp(url, function (err, data) {
-    console.log(err);
-    if (err) list.innerHTML = templates.error();
+    if (err) {
+      console.log(err);
+      list.innerHTML = templates.error();
+    }
     else if (data.count === 0) list.innerHTML = templates.noResults({ name: decodeURIComponent(animal) });
     else if (data.count > 0) list.innerHTML = templates.results({ results: data.results });
   });
