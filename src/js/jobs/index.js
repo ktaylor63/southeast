d7ee2e43cb4d6ccc0dbe7cf6389733b2d4450901
Roy Hewitt <roy_hewitt@fws.gov>
2016-11-17 13:51:12 -0500

    If the search for jobs in the Southeast doesn't return any results expand the search nationwide

--- src/js/jobs/index.js
+++ src/js/jobs/index.js
@@ -10,8 +10,6 @@
 
   var output  = document.querySelector('.card-list');
   var API_KEY = '84OIdzD2Zota1B7hzOQmuWydt/kh8k1z2bdCx6dHpBY=';
-  var EMAIL   = 'roy_hewitt@fws.gov';
-  var HOST    = 'data.usajobs.gov';
   var baseURL = 'https://data.usajobs.gov/api/search';
   var parameters = {
     Organization: 'IN15',// Get results for USFWS only
@@ -30,24 +28,47 @@
   };
 
   xhr(options, function(err, res, body) {
-    if (err) {
-      output.innerHTML = templates.error({
-        error: 'Could not download jobs listings',
-        message: 'Visit <a href="https://usajobs.gov">USAjobs.gov</a> to create your own search.'
-      });
-    }
+    if (err) expandSearch();
 
     if (!err && res.statusCode == 200) {
       var data = JSON.parse(body);
-      if (data.SearchResult.SearchResultCount === 0) {
-        output.innerHTML = templates.error({
-          error: 'No U.S. Fish and Wildlife job openings found in the Southeast Region.',
-          message: 'Try <a href="https://usajobs.gov">USAjobs.gov</a> to expand your search criteria.'
-        });
-      } else {
-        output.innerHTML = templates.results({ results: data.SearchResult.SearchResultItems });
-      }
+      if (data.SearchResult.SearchResultCount === 0) expandSearch();
+      else render('results', { results: data.SearchResult.SearchResultItems });
     }
   });
+
+  function render(template, data) {
+    output.innerHTML = templates[template](data);
+  }
+
+  function expandSearch() {
+    var h2 = document.createElement('h2');
+    h2.innerHTML = 'No Results in the Southeast, Expanding Search Nationwide:';
+    queryString = qs.stringify({ Organization: 'IN15' });
+    options.url = [baseURL, queryString].join('?');
+
+    xhr.get(options, function (err, res, body) {
+      if (err) {
+        render('error', {
+          error: 'Could not download jobs listings',
+          message: 'Visit <a href="https://usajobs.gov">USAjobs.gov</a> to create your own search.'
+        });
+      }
+
+      if (!err && res.statusCode == 200) {
+        var data = JSON.parse(body);
+        if (data.SearchResult.SearchResultCount === 0) {
+          render('error', {
+            error: 'No U.S. Fish and Wildlife job openings found.',
+            message: 'Try <a href="https://usajobs.gov">USAjobs.gov</a> to expand your search criteria.'
+          });
+        } else {
+          render('results', { results: data.SearchResult.SearchResultItems });
+          output.parentNode.insertBefore(h2, output);
+        }
+      }
+    })
+
+  }
 
 })();
