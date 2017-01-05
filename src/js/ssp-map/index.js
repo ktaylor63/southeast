2d51a0bd03a459931d73f65d8a1238f0a61a1fe4
Roy Hewitt <roy_hewitt@fws.gov>
2017-01-05 09:14:04 -0500

    Allow users to download data from SSP story map

--- src/js/ssp-map/index.js
+++ src/js/ssp-map/index.js
@@ -2,11 +2,12 @@
   'use strict';
 
   var L = require('leaflet');
+  var clickDownload = require('client-csv');
+  var papaparse = require('papaparse');
+  require('leaflet-draw');
 
   var _ = {
-    hasClass: require('has-class'),
-    removeClass: require('remove-class'),
-    addClass: require('add-class')
+    inside: require('turf-inside')
   };
 
   var template = require('./project-template.jade');
@@ -14,9 +15,17 @@
   var layers = require('./layers');
 
   var infoWindow = document.querySelector('.info-window');
+  var downloadButton = document.querySelector('.download-button');
   var infoContent = infoWindow.querySelector('.info-window-content');
   var markerPath = document.body.getAttribute('data-root') + 'images/';
   L.Icon.Default.imagePath = markerPath;
+
+  var editableLayers = new L.FeatureGroup();
+  var drawOptions = {
+    draw: { circle: false, polyline: false, rectangle: false, marker: false },
+    edit: { featureGroup: editableLayers, edit: false }
+  };
+  var drawControl = new L.Control.Draw(drawOptions);
 
   var map, projects;
 
@@ -33,8 +42,48 @@
       onEachFeature: clickHandler
     }).addTo(map);
 
-    map.fitBounds(geojson.getBounds());
+    map
+      .addLayer(editableLayers)
+      .fitBounds(geojson.getBounds())
+      .addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, function (e) {
+      var markersInPolygon = pointsInPolygon(geojson, e.layer.toGeoJSON() );
+      if (markersInPolygon.length === 0) return;
+      var data = normalizeDataForDownload(markersInPolygon);
+      createDownloadLink(data);
+      editableLayers.addLayer(e.layer);
+    });
     layers.esriTopo.addTo(map);
+  }
+
+  // Use Turf.js inside function to return an array of markers that
+  //  fall inside the user-created polygon
+  function pointsInPolygon(points, polygon) {
+    var allMarkers = Object.values(points._layers);
+    return allMarkers.filter( function(marker) {
+      return _.inside(marker.toGeoJSON(), polygon);
+    });
+  }
+
+  function normalizeDataForDownload(markers) {
+    return markers.map(function(marker) {
+      var normalized = marker.feature.properties;
+      normalized.lat = marker.feature.geometry.coordinates[0];
+      normalized.lon = marker.feature.geometry.coordinates[1];
+      return normalized;
+    });
+  }
+
+  function createDownloadLink(data) {
+    var string = papaparse.unparse(data);
+    clickDownload(downloadButton, function(encode) {
+      return {
+        filename: 'ssp-projects.csv',
+        contents: encode.text(string)
+      };
+    });
+    downloadButton.classList.add('active');
   }
 
   function clickHandler(feature, layer) {
@@ -44,9 +93,7 @@
   function toggleInfoWindow(e) {
     var project = e.target.feature;
     infoContent.innerHTML = template({ project: project });
-    var activeClass = 'active';
-    if ( _.hasClass(infoWindow, activeClass) ) _.removeClass(infoWindow, activeClass);
-    else _.addClass(infoWindow, activeClass);
+    infoWindow.classList.toggle('active');
   }
 
   init();
