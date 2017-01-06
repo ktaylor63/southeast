769581a164ce721cd6d232479f31634c76747c48
Roy Hewitt <roy_hewitt@fws.gov>
2017-01-06 09:38:49 -0500

    Create all of the directories in the dist folder that we need to build

--- build/create-dist.js
+++ build/create-dist.js
@@ -3,7 +3,14 @@
 
   var mkdirp = require('mkdirp');
 
-  var directories = ['dist/js', 'dist/data', 'dist/css', 'site/static/data'];
+  var directories = [
+    'dist/js',
+    'dist/data',
+    'dist/css',
+    'dist/images/hero',
+    'dist/images/pages',
+    'data/data'
+  ];
 
   directories.forEach(function (path) {
     mkdirp(path, function (err) {
