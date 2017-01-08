891a20c02694b693fb933786feaa29399d98d145
Roy Hewitt <roy_hewitt@fws.gov>
2017-01-08 15:41:02 -0500

    Move logic pertaining to cleaning dist to the startup creat dist script

--- build/hugo.js
+++ build/hugo.js
@@ -1,31 +1,23 @@
 (function () {
   'use strict';
 
-  const rimraf = require('rimraf');
   const spawn = require('child_process').spawn;
-
   const baseURL = process.argv[3];
 
-  // Don't blast away CSS, JavaScript, or Images or we'll have to just re-copy them
-  const options = { glob: { ignore: ['dist/css', 'dist/js', 'dist/images', 'dist/data'] } };
+  var hugo = spawn('hugo', [
+    '--canonifyURLs=true',
+    '--config=site/config.yml',
+    '--destination=../dist/',
+    '--source=site/',
+    `--baseURL=${baseURL}`
+  ]);
 
-  rimraf('dist/*', options, function (err) {
-    if (err) console.error(err);
-    var hugo = spawn('hugo', [
-      '--canonifyURLs=true',
-      '--config=site/config.yml',
-      '--destination=../dist/',
-      '--source=site/',
-      `--baseURL=${baseURL}`
-    ]);
+  hugo.stdout.on('data', function (data) {
+    console.log(data.toString('utf8'));
+  });
 
-    hugo.stdout.on('data', function (data) {
-      console.log(data.toString('utf8'));
-    });
-
-    hugo.stderr.on('data', function (data) {
-      console.error(data.toString('utf8'));
-    });
+  hugo.stderr.on('data', function (data) {
+    console.error(data.toString('utf8'));
   });
 
 })();
