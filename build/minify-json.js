7acf12bfb924d60640f0873715cc6f12aefbfd13
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-06 08:07:45 -0500

    Remove individual watcher, create async build function

--- build/minify-json.js
+++ build/minify-json.js
@@ -1,55 +1,54 @@
-(function () {
-  'use strict';
+const jsonminify = require('jsonminify');
+const jsonlint = require('jsonlint');
+const rimraf = require('rimraf');
+const glob = require('glob');
+const each = require('async/each');
 
-  var jsonminify = require('jsonminify');
-  var jsonlint = require('jsonlint');
-  var chokidar = require('chokidar');
-  var rimraf = require('rimraf');
-  var glob = require('glob');
+const fs = require('fs');
+const path = require('path');
 
-  var fs = require('fs');
-  var path = require('path');
+const jsonSrc = 'src/data/**/*.json';
+const dist = 'dist/data/';
 
-  var jsonSrc = 'src/data/**/*.json';
-  var dist = 'dist/data/';
+build(err => {
+  if (err) console.log(err);
+  console.log('Finished minifying JSON data.');
+})
 
-  init();
+function build(done) {
+  glob(jsonSrc, (err, files) => {
+    if (err) return done(err);
+    each(files, minifyJSON, done);
+  });
+}
 
-  function init() {
-    if (process.env.WATCH) {
-      chokidar.watch(jsonSrc)
-        .on('add', minifyJson)
-        .on('change', minifyJson)
-        .on('unlink', removeJson)
-    } else {
-      rimraf(dist + '*.js', function() {
-        glob(jsonSrc, function (err, files) {
-          if (err) console.error(err);
-          files.forEach(minifyJson);
-        });
+function minifyJSON(filepath, cb) {
+  fs.readFile(filepath, 'utf8', (err, json) => {
+    if (err) console.log(err);
+
+    if (jsonlint.parse(json)) {
+      const basename = path.basename(filepath).replace('json', 'js');
+      const outpath = path.join(dist, basename);
+      fs.writeFile(outpath, jsonminify(json, 'utf8'), (err) => {
+        if (err) console.log(err);
+        if (cb) cb();
+      });
+    } else throw new Error(basename + ' is not valid JSON.')
+  });
+}
+
+function removeJSON(filepath) {
+  const fileToDelete = path.join( dist + path.basename(filepath).replace('json', 'js') );
+  fs.access(fileToDelete, (err) => {
+    if (err) console.error(err);
+    else {
+      fs.unlink(fileToDelete, (err) => {
+        if (err) console.error(err);
       });
     }
-  }
+  });
+}
 
-  function minifyJson(file) {
-    fs.readFile(file, 'utf8', function(err, json) {
-      if (jsonlint.parse(json)) {
-        var basename = path.basename(file).replace('json', 'js');
-        fs.writeFile(dist + basename, jsonminify(json, 'utf8'));
-      } else throw new Error(basename + ' is not valid JSON.')
-    });
-  }
-
-  function removeJson(filepath) {
-    var fileToDelete = path.join( dist + path.basename(filepath).replace('json', 'js') );
-    fs.access(fileToDelete, function(err) {
-      if (err) console.error(err);
-      else {
-        fs.unlink(fileToDelete, function(err) {
-          if (err) console.error(err);
-        });
-      }
-    });
-  }
-
-})();
+module.exports.removeJSON = removeJSON;
+module.exports.minifyJSON = minifyJSON;
+module.exports.build = build;
