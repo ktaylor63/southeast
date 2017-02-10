42645b223100ebf2e948a48a62682c1fc5317056
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-10 13:29:07 -0500

    Update contact list to include field station, use ES6

--- src/js/every-page/contacts.js
+++ src/js/every-page/contacts.js
@@ -1,17 +1,16 @@
 (function () {
   'use strict';
 
-  var xhr = require('xhr');
-  var filter = require('lodash.filter');
+  const xhr = require('xhr');
 
-  var template = require('./contact.jade');
-  var baseUrl = document.body.getAttribute('data-root');
-  var url = baseUrl + '/data/contacts.js';
+  const template = require('./contact.pug');
+  const baseURL = document.body.getAttribute('data-root');
+  const url = `${baseURL}data/contacts.js`;
 
-  var input = document.querySelector('.contact-list-search');
-  var output = document.querySelector('.contact-list');
+  const input = document.querySelector('.contact-list-search');
+  const output = document.querySelector('.contact-list');
 
-  var contacts;
+  let contacts;
 
   input.addEventListener('keyup', search);
 
@@ -20,7 +19,7 @@
   }
 
   function downloadContacts(url, cb) {
-    xhr.get(url, function (err, res, body) {
+    xhr.get(url, (err, res, body) => {
       if (err) console.log(err);
       contacts = JSON.parse(body);
       cb(contacts);
@@ -28,17 +27,17 @@
   }
 
   function search(e) {
-    var query = e.target.value.toLowerCase(),
-        filtered;
+    var query = e.target.value;
+    var regex = new RegExp(query, 'gi');
 
     if (query.length === 0) render(contacts);
 
-    filtered = filter(contacts, function (person) {
-      var isType = person.type.toLowerCase().indexOf(query) > -1;
-      var isState = person.state.toLowerCase().indexOf(query) > -1;
-      var isName = person.name.toLowerCase().indexOf(query) > -1;
-      var isTitle = person.title.toLowerCase().indexOf(query) > -1;
-      var isStation = person.station.toLowerCase().indexOf(query) > -1;
+    const filtered = contacts.filter(person => {
+      const isType = regex.test(person.type);
+      const isState = regex.test(person.state);
+      const isName = regex.test(person.name);
+      const isTitle = regex.test(person.title);
+      const isStation = regex.test(person.station);
       return (isType || isState || isName || isTitle || isStation);
     });
 
@@ -46,7 +45,8 @@
   }
 
   function render(contacts) {
-    output.innerHTML = template({ contacts: contacts, getPhone: normalizePhoneNumber });
+    const sortedContacts = contacts.slice(0).sort(sortByStation);
+    output.innerHTML = template({ contacts: sortedContacts, getPhone: normalizePhoneNumber });
   }
 
   function normalizePhoneNumber(number) {
@@ -58,6 +58,20 @@
 
   function insert(str, index, value) {
     return str.substr(0, index) + value + str.substr(index);
+  }
+
+  function sortByName(a,b) {
+    const aName = (a.name) ? a.name : a.station;
+    const bName = (b.name) ? b.name : b.station;
+    if (aName.toLowerCase() > bName.toLowerCase()) return 1;
+    if (aName.toLowerCase() < bName.toLowerCase()) return -1;
+    return 0;
+  }
+
+  function sortByStation(a,b) {
+    if (a.station.toLowerCase() > b.station.toLowerCase()) return 1;
+    if (a.station.toLowerCase() < b.station.toLowerCase()) return -1;
+    return 0;
   }
 
   module.exports.init = init;
