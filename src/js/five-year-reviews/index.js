cf2600d5aa58b175deaf80b68d838ee24519ce48
Roy Hewitt <roy_hewitt@fws.gov>
2017-03-22 08:21:03 -0400

    Remove Pug -> template strings

--- src/js/five-year-reviews/index.js
+++ src/js/five-year-reviews/index.js
@@ -1,49 +1,52 @@
-(function () {
-  'use strict';
+const moment = require('moment');
+const xhr = require('xhr');
 
-  const fs = require('fs');
-  const moment = require('moment');
-  const xhr = require('xhr');
+const list = document.querySelector('.five-year-review-list');
+const input = document.querySelector('.five-year-review-search');
 
-  const template = require('./li.pug');
+const baseURL = document.body.getAttribute('data-root');
+const url = `${baseURL}data/five-year-reviews.js`;
+let species;
 
-  const list = document.querySelector('.five-year-review-list');
-  const input = document.querySelector('.five-year-review-search');
+xhr.get(url, (err, res, body) => {
+  if (err) console.log(err);
+  species = JSON.parse(body);
+  render(species);
+  input.addEventListener('keyup', search);
+});
 
-  const baseURL = document.body.getAttribute('data-root');
-  const url = `${baseURL}data/five-year-reviews.js`;
-  let species;
+function search(e) {
+  const query = e.target.value;
+  const regex = new RegExp(query, 'gi');
+  if (query.length === 0) render(species);
 
-  xhr.get(url, (err, res, body) => {
-    if (err) console.log(err);
-    species = JSON.parse(body);
-    render(species);
-    input.addEventListener('keyup', search);
+  const filtered = species.filter(animal => {
+    const isName = regex.test(animal.commonName);
+    const isStatus = regex.test(animal.status);
+    const isTaxon = regex.test(animal.taxon);
+    return (isName || isStatus || isTaxon);
   });
+  render(filtered);
+}
 
-  function search(e) {
-    const query = e.target.value;
-    const regex = new RegExp(q, 'gi');
-    if (query.length === 0) render(species);
+function createListItem(species) {
+  const date = moment(species.reviewDate).format('MM/DD/YYYY');
+  const url = [baseURL, species.url].join('');
+  return species.url ?
+    `<li>${date}: <a href="${url}" target="_blank">${species.commonName} (${species.status})</a></li>` :
+    `<li>${date}: ${species.commonName} (${species.status})</li>`;
+}
 
-    const filtered = species.filter(animal => {
-      const isName = regex.test(animal.commonName);
-      const isStatus = regex.test(animal.status);
-      const isTaxon = regex.test(animal.taxon);
-      return (isName || isStatus || isTaxon);
-    });
-    render(filtered);
-  }
+function render(species) {
+  list.innerHTML = species
+    .slice()
+    .sort(sortByDate)
+    .map(createListItem)
+    .join('');;
+}
 
-  function render(species) {
-    const sorted = species.slice().sort(sortByDate);
-    list.innerHTML = template({ species: sorted });
-  }
-
-  function sortByDate(a, b) {
-    if (new Date(a.reviewDate) > new Date(b.reviewDate)) return -1;
-    if (new Date(a.reviewDate) < new Date(b.reviewDate)) return 1;
-    return 0;
-  }
-
-})();
+function sortByDate(a, b) {
+  if (new Date(a.reviewDate) > new Date(b.reviewDate)) return -1;
+  if (new Date(a.reviewDate) < new Date(b.reviewDate)) return 1;
+  return 0;
+}
