d66f4d634d04a4d85f7bf53406fb118ea67f01e6
Roy Hewitt <roy_hewitt@fws.gov>
2017-03-27 14:33:03 -0400

    Build drafts on dev server

--- build/hugo.js
+++ build/hugo.js
@@ -2,13 +2,16 @@ const spawn = require('child_process').spawn;
 
 function build(baseURL) {
   baseURL = (baseURL) ? baseURL : process.argv[2];
-  const hugo = spawn('hugo', [
+  const args = [
     '--canonifyURLs=true',
     '--config=site/config.yml',
     '--destination=../dist/',
     '--source=site/',
     `--baseURL=${baseURL}`
-  ]);
+  ];
+  if (baseURL.includes('localhost')) args.push('--buildDrafts')
+
+  const hugo = spawn('hugo', args);
 
   hugo.stdout.on('data', (data) => console.log(data.toString('utf8')) );
 
