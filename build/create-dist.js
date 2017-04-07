febc97b3fc5734325f73a8f9890cdd000c6add03
Roy Hewitt <roy_hewitt@fws.gov>
2017-04-07 11:37:43 -0400

    Nuke dist folder before building site for deploy

--- build/create-dist.js
+++ build/create-dist.js
@@ -1,28 +1,23 @@
-(function () {
-  'use strict';
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
-  const mkdirp = require('mkdirp');
-  const rimraf = require('rimraf');
+const directories = [
+  'dist/js',
+  'dist/data',
+  'dist/css',
+  'dist/images/hero',
+  'dist/images/pages'
+];
 
-  const directories = [
-    'dist/js',
-    'dist/data',
-    'dist/css',
-    'dist/images/hero',
-    'dist/images/pages'
-  ];
+// Don't blast away CSS, JavaScript, or Images or we'll have to just re-copy them
+const options = { glob: { ignore: ['dist/css', 'dist/js', 'dist/images', 'dist/data'] } };
 
-  // Don't blast away CSS, JavaScript, or Images or we'll have to just re-copy them
-  const options = { glob: { ignore: ['dist/css', 'dist/js', 'dist/images', 'dist/data'] } };
+rimraf('dist/*', options, function (err) {
+  if (err) console.error(err);
 
-  rimraf('dist/*', options, function (err) {
-    if (err) console.error(err);
-
-    directories.forEach(function (path) {
-      mkdirp(path, function (err) {
-        if (err) console.error(err);
-      });
+  directories.forEach(function (path) {
+    mkdirp(path, function (err) {
+      if (err) console.error(err);
     });
   });
-
-})();
+});
