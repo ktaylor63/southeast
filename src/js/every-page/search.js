c8daf5c0e7b3623341bbf3d5370fd02fcf38a9c0
Roy Hewitt <roy_hewitt@fws.gov>
2017-07-20 11:22:37 -0400

    Download search index only when the search component is toggled/used.

--- src/js/every-page/search.js
+++ src/js/every-page/search.js
@@ -9,10 +9,12 @@ let options = {
   active: false,
   className: 'active',
   container,
-  close: container.querySelector('.close')
+  close: container.querySelector('.close'),
+  downloaded: false
 };
 
 const toggle = () => {
+  if (!options.downloaded) downloadData();
   options.container.classList.toggle(options.className);
   options.active = !options.active;
   if (options.active) options.input.focus();
@@ -25,16 +27,8 @@ let pages;
 let results;
 let index;
 
-const createNewsUrl = (url, date) => {
-  if (!url || !date) return;
-  const urlPieces = url.split('/');
-  const datePieces = date.split('-');
-  return [urlPieces[0], datePieces[0], datePieces[1], urlPieces[1]].join('/');
-}
-
 const createListItem = p => {
   const sec = S(p.section).replaceAll('-', ' ').s;
-  const url = S(p.uri).contains('news') ? createNewsUrl(p.uri, p.date) : p.uri;
   const img = p.img ? `<img src="${baseURL}images/hero/small/${p.img}" alt="${p.alt}" />` : '';
   const section = p.date ?
     `<span class="content-type">${sec}</span> | ${moment(p.date).format('MMM D, YYYY')}` :
@@ -47,8 +41,8 @@ const createListItem = p => {
           <p>${section}</p>
         </div>
         <div class="search-item-content-text">
-          <h3><a href=${url}>${ p.title }</a></h3>
-          <p>${p.summary} <a href="${url}">Read more...</a></p>
+          <h3><a href=${p.uri}>${ p.title }</a></h3>
+          <p>${p.summary} <a href="${p.uri}">Read more...</a></p>
         </div>
       </div>
     </li>`;
@@ -65,6 +59,7 @@ const closeSearchComponent = e => {
 }
 
 const search = e => {
+  if (!options.downloaded) return downloadData();
   const query = e.target.value;
 
   if (query.length === 0) render(pages);
@@ -79,9 +74,15 @@ const search = e => {
 const init = (input, dataURL) => {
   results = document.querySelector('.site-wide-search-results');
   options.input = input;
+  window.addEventListener('keyup', closeSearchComponent);
+  input.addEventListener('input', search);
+  options.close.addEventListener('click', toggle);
+}
 
+function downloadData() {
   xhr.get(`${baseURL}data/search.js`, (err, res, body) => {
-    if (err) console.log(err);
+    if (err) return console.log(err);
+    options.downloaded = true;
     pages = JSON.parse(body)
       .map((p, i) => {
         return {
@@ -112,9 +113,7 @@ const init = (input, dataURL) => {
       this.ref('id');
       pages.forEach(p => this.add(p), this);
     });
-    window.addEventListener('keyup', closeSearchComponent);
-    input.addEventListener('keyup', search);
-    options.close.addEventListener('click', toggle);
+    search({ target: { value: '' }});
   });
 }
 
