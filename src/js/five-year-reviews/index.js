d6db92a6abe455143d435848ce20476f8a88749d
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-22 12:24:38 -0400

    Add 5 year review documents, include and display a new 'recommendation' field

--- src/js/five-year-reviews/index.js
+++ src/js/five-year-reviews/index.js
@@ -5,18 +5,27 @@ const list = document.querySelector('.five-year-review-list');
 const input = document.querySelector('.five-year-review-search');
 
 let hasWWW = window.location.href.indexOf('www');
-hasWWW = (hasWWW < 0) ? false : true;
+hasWWW = !(hasWWW < 0);
 const baseURL = document.body.getAttribute('data-root');
 const dataURL = hasWWW ? baseURL : baseURL.replace('www.', '');
 
 let species;
 
-xhr.get(`${dataURL}data/five-year-reviews.js`, (err, res, body) => {
-  if (err) console.log(err);
-  species = JSON.parse(body);
-  render(species);
-  input.addEventListener('keyup', search);
-});
+function sortByDate(a, b) {
+  return new Date(b.reviewDate) - new Date(a.reviewDate);
+}
+
+function createListItem(animal) {
+  const date = moment(animal.reviewDate).format('MM/DD/YYYY');
+  const url = [baseURL, animal.url].join('');
+  return animal.url
+    ? `<li>${date}: <a href="${url}" target="_blank">${animal.commonName} (${animal.status})</a><br><strong>Recommendation: ${animal.recommendation}</strong></li>`
+    : `<li>${date}: ${animal.commonName} (${animal.status})<br><strong>Recommendation: ${animal.recommendation}</strong></li>`;
+}
+
+function render(animals) {
+  list.innerHTML = animals.slice().sort(sortByDate).map(createListItem).join('');
+}
 
 function search(e) {
   const query = e.target.value;
@@ -27,29 +36,14 @@ function search(e) {
     const isName = regex.test(animal.commonName);
     const isStatus = regex.test(animal.status);
     const isTaxon = regex.test(animal.taxon);
-    return (isName || isStatus || isTaxon);
+    return isName || isStatus || isTaxon;
   });
   render(filtered);
 }
 
-function createListItem(species) {
-  const date = moment(species.reviewDate).format('MM/DD/YYYY');
-  const url = [baseURL, species.url].join('');
-  return species.url ?
-    `<li>${date}: <a href="${url}" target="_blank">${species.commonName} (${species.status})</a></li>` :
-    `<li>${date}: ${species.commonName} (${species.status})</li>`;
-}
-
-function render(species) {
-  list.innerHTML = species
-    .slice()
-    .sort(sortByDate)
-    .map(createListItem)
-    .join('');;
-}
-
-function sortByDate(a, b) {
-  if (new Date(a.reviewDate) > new Date(b.reviewDate)) return -1;
-  if (new Date(a.reviewDate) < new Date(b.reviewDate)) return 1;
-  return 0;
-}
+xhr.get(`${dataURL}data/five-year-reviews.js`, (err, res, body) => {
+  if (err) console.log(err);
+  species = JSON.parse(body);
+  render(species);
+  input.addEventListener('keyup', search);
+});
