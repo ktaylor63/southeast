27284a936cb31de4538a73ce3d29dd43f0fdfcdc
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-23 14:53:38 -0400

    Add momentjs to dependencies, prettier style

--- src/js/every-page/search.js
+++ src/js/every-page/search.js
@@ -4,8 +4,13 @@ const S = require('string');
 const moment = require('moment');
 
 const container = document.querySelector('.site-wide-search');
+const baseURL = document.body.getAttribute('data-root');
 
-let options = {
+let pages;
+let results;
+let index;
+
+const options = {
   active: false,
   className: 'active',
   container,
@@ -13,26 +18,12 @@ let options = {
   downloaded: false
 };
 
-const toggle = () => {
-  if (!options.downloaded) downloadData();
-  options.container.classList.toggle(options.className);
-  options.active = !options.active;
-  if (options.active) options.input.focus();
-}
-
-const baseURL = document.body.getAttribute('data-root');
-const refNode = document.querySelector('.site-search-button');
-
-let pages;
-let results;
-let index;
-
 const createListItem = p => {
   const sec = S(p.section).replaceAll('-', ' ').s;
   const img = p.img ? `<img src="${baseURL}images/hero/small/${p.img}" alt="${p.alt}" />` : '';
-  const section = p.date ?
-    `<span class="content-type">${sec}</span> | ${moment(p.date).format('MMM D, YYYY')}` :
-    `<span class="content-type">${sec}</span>`;
+  const section = p.date
+    ? `<span class="content-type">${sec}</span> | ${moment(p.date).format('MMM D, YYYY')}`
+    : `<span class="content-type">${sec}</span>`;
   return `
     <li class="site-wide-search-item">
       <div class="search-item-content">
@@ -41,65 +32,35 @@ const createListItem = p => {
           <p>${section}</p>
         </div>
         <div class="search-item-content-text">
-          <h3><a href=${p.uri}>${ p.title }</a></h3>
+          <h3><a href=${p.uri}>${p.title}</a></h3>
           <p>${p.summary} <a href="${p.uri}">Read more...</a></p>
         </div>
       </div>
     </li>`;
-}
+};
 
 const render = hits => {
-  const html = hits.map(createListItem).join('');
-  results.innerHTML = html;
-}
-
-const closeSearchComponent = e => {
-  const key = e.which || e.keyCode;
-  if (key === 27 && options.active) toggle();
-}
-
-const search = e => {
-  if (!options.downloaded) return downloadData();
-  const query = e.target.value;
-
-  if (query.length === 0) render(pages);
-
-  const hits = index
-    .search(query)
-    .map(h => pages[h.ref]);
-
-  render(hits);
-}
-
-const init = (input, dataURL) => {
-  results = document.querySelector('.site-wide-search-results');
-  options.input = input;
-  window.addEventListener('keyup', closeSearchComponent);
-  input.addEventListener('input', search);
-  options.close.addEventListener('click', toggle);
-}
+  results.innerHTML = hits.map(createListItem).join('');
+};
 
 function downloadData() {
   xhr.get(`${baseURL}data/search.js`, (err, res, body) => {
     if (err) return console.log(err);
     options.downloaded = true;
-    pages = JSON.parse(body)
-      .map((p, i) => {
-        return {
-          title: p.title,
-          summary: p.summary,
-          tags: p.tags,
-          uri: p.uri,
-          section: p.section,
-          date: p.date,
-          updated: p.updated,
-          alt: p.alt,
-          caption: p.caption,
-          img: p.img,
-          id: i
-        }
-      });
-    index = lunr(function() {
+    pages = JSON.parse(body).map((p, i) => ({
+      title: p.title,
+      summary: p.summary,
+      tags: p.tags,
+      uri: p.uri,
+      section: p.section,
+      date: p.date,
+      updated: p.updated,
+      alt: p.alt,
+      caption: p.caption,
+      img: p.img,
+      id: i
+    }));
+    index = lunr(function setupIndex() {
       this.field('title', { boost: 10 });
       this.field('summary');
       this.field('tags', { boost: 5 });
@@ -113,9 +74,40 @@ function downloadData() {
       this.ref('id');
       pages.forEach(p => this.add(p), this);
     });
-    search({ target: { value: '' }});
+    return search({ target: { value: '' } });
   });
 }
+
+const search = e => {
+  if (!options.downloaded) return downloadData();
+  const query = e.target.value;
+
+  if (query.length === 0) render(pages);
+
+  const hits = index.search(query).map(h => pages[h.ref]);
+
+  return render(hits);
+};
+
+const toggle = () => {
+  if (!options.downloaded) downloadData();
+  options.container.classList.toggle(options.className);
+  options.active = !options.active;
+  if (options.active) options.input.focus();
+};
+
+const closeSearchComponent = e => {
+  const key = e.which || e.keyCode;
+  if (key === 27 && options.active) toggle();
+};
+
+const init = input => {
+  results = document.querySelector('.site-wide-search-results');
+  options.input = input;
+  window.addEventListener('keyup', closeSearchComponent);
+  input.addEventListener('input', search);
+  options.close.addEventListener('click', toggle);
+};
 
 module.exports.init = init;
 module.exports.toggle = toggle;
