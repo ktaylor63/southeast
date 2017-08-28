78542f0ba8b6cb9ac33b0a589037a8e84b313617
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-28 07:26:46 -0400

    Remove commented code, es6 shorthand property name, install xhr

--- src/js/ssp/index.js
+++ src/js/ssp/index.js
@@ -5,7 +5,7 @@ const random = require('random-item');
 const template = require('./project.jade');
 
 const output = document.querySelector('.featured-project');
-const baseURL = 'https://www.sciencebase.gov/catalog/items';
+const baseURL = 'https://cors-anywhere.herokuapp.com/https://www.sciencebase.gov/catalog/items';
 
 const params = qs.stringify({
   format: 'json',
@@ -17,28 +17,11 @@ const params = qs.stringify({
 // Join the BaseURL with query string
 const url = [baseURL, params].join('?');
 
-xhr.get(url, handleResponse);
-
 function handleResponse(err, res, body) {
   if (err) console.log(err);
   const data = JSON.parse(body);
   const project = random(data.items);
-  output.innerHTML = template({ project: project });
+  output.innerHTML = template({ project });
 }
 
-// function template(project) {
-//   let img = '';
-//   if (project.previewImage.original) {
-//     img = `
-//       <figure class="photo-right">
-//         <img src=${project.previewImage.original.viewUri} alt="Preview image for this project" />
-//       </figure>
-//     `
-//   }
-//   return `
-//     <h4><a href="${project.link.url}" target="_blank">${project.title}</a></h4>
-//     ${img}
-//     <p>${project.body}</p>
-//   `;
-//
-// }
+xhr.get(url, handleResponse);
