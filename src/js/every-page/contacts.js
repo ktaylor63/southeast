c622a107fcea16201d204932d6416fa891090303
Roy Hewitt <roy_hewitt@fws.gov>
2017-10-16 10:32:30 -0400

    Bump scrollnav version, fix eslint errors

--- src/js/every-page/contacts.js
+++ src/js/every-page/contacts.js
@@ -1,29 +1,42 @@
 const xhr = require('xhr');
 
 const template = require('./contact.pug');
+
 const input = document.querySelector('.contact-list-search');
 const output = document.querySelector('.contact-list');
 
 let hasWWW = window.location.href.indexOf('www');
-hasWWW = (hasWWW < 0) ? false : true;
+hasWWW = !(hasWWW < 0);
 const baseURL = document.body.getAttribute('data-root');
 const dataURL = hasWWW ? baseURL : baseURL.replace('www.', '');
 
 let contacts;
 
-input.addEventListener('keyup', search);
+function insert(str, index, value) {
+  return str.substr(0, index) + value + str.substr(index);
+}
 
-function init() {
-  xhr.get(`${dataURL}/data/contacts.js`, (err, res, body) => {
-    if (err) console.log(err);
-    contacts = JSON.parse(body);
-    render(contacts);
-  });
+function normalizePhoneNumber(number) {
+  let phone = number.replace(/\D/g, '');
+  // This pauses for a second to dial an extension
+  if (phone.length > 10) phone = insert(phone, 10, 'p');
+  return `tel:+1${phone}`;
+}
+
+function sortByStation(a, b) {
+  if (a.station.toLowerCase() > b.station.toLowerCase()) return 1;
+  if (a.station.toLowerCase() < b.station.toLowerCase()) return -1;
+  return 0;
+}
+
+function render(theContacts) {
+  const sortedContacts = theContacts.slice(0).sort(sortByStation);
+  output.innerHTML = template({ contacts: sortedContacts, getPhone: normalizePhoneNumber });
 }
 
 function search(e) {
-  var query = e.target.value;
-  var regex = new RegExp(query, 'gi');
+  const query = e.target.value;
+  const regex = new RegExp(query, 'gi');
 
   if (query.length === 0) render(contacts);
 
@@ -33,40 +46,19 @@ function search(e) {
     const isName = regex.test(person.name);
     const isTitle = regex.test(person.title);
     const isStation = regex.test(person.station);
-    return (isType || isState || isName || isTitle || isStation);
+    return isType || isState || isName || isTitle || isStation;
   });
 
   render(filtered);
 }
 
-function render(contacts) {
-  const sortedContacts = contacts.slice(0).sort(sortByStation);
-  output.innerHTML = template({ contacts: sortedContacts, getPhone: normalizePhoneNumber });
-}
-
-function normalizePhoneNumber(number) {
-  let phone = number.replace(/\D/g,'');
-  // This pauses for a second to dial an extension
-  if (phone.length > 10) phone = insert(phone, 10, 'p');
-  return 'tel:+1' + phone;
-}
-
-function insert(str, index, value) {
-  return str.substr(0, index) + value + str.substr(index);
-}
-
-function sortByName(a,b) {
-  const aName = (a.name) ? a.name : a.station;
-  const bName = (b.name) ? b.name : b.station;
-  if (aName.toLowerCase() > bName.toLowerCase()) return 1;
-  if (aName.toLowerCase() < bName.toLowerCase()) return -1;
-  return 0;
-}
-
-function sortByStation(a,b) {
-  if (a.station.toLowerCase() > b.station.toLowerCase()) return 1;
-  if (a.station.toLowerCase() < b.station.toLowerCase()) return -1;
-  return 0;
+function init() {
+  xhr.get(`${dataURL}/data/contacts.js`, (err, res, body) => {
+    if (err) console.log(err);
+    contacts = JSON.parse(body);
+    input.addEventListener('keyup', search);
+    render(contacts);
+  });
 }
 
 module.exports.init = init;
