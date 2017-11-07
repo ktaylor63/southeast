babe71f9766c96e68b56a4eab2fa132a3e141491
Roy Hewitt <roy_hewitt@fws.gov>
2017-11-07 15:23:45 -0500

    Add --buildFuture option when we're not in production

--- build/hugo.js
+++ build/hugo.js
@@ -2,18 +2,21 @@ const exec = require('child_process').exec;
 const endOfLine = require('os').EOL;
 const chalk = require('chalk');
 
+const liveBaseUrl = 'fws.gov/southeast';
+
 function build(baseURL) {
   const base = baseURL || process.argv[2];
   console.log(chalk.yellow(`Base URL: ${base} ${endOfLine}`));
-  const buildDrafts = baseURL.includes('fws.gov/southeast')
-    ? '--buildDrafts=false'
-    : '--buildDrafts=true';
+  const buildDrafts = baseURL.includes(liveBaseUrl) ? '--buildDrafts=false' : '--buildDrafts=true';
+  const buildFuture = baseURL.includes(liveBaseUrl) ? '--buildFuture=false' : '--buildFuture=true';
   const command = [
     'hugo',
+    '--buildFuture=true',
     '--config=site/config.yml',
     '--source=site/',
     `--baseURL=${base}`,
-    buildDrafts
+    buildDrafts,
+    buildFuture
   ].join(' ');
 
   exec(command, (err, stdout, stderr) => {
