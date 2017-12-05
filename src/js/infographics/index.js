d68f8a7e725aa192aa869d3b40267f044b96d2a3
Roy Hewitt <roy_hewitt@fws.gov>
2017-12-05 07:37:50 -0500

    Wait for images to load before initializing the masonry layout

--- src/js/infographics/index.js
+++ src/js/infographics/index.js
@@ -1,7 +1,12 @@
 const Masonry = require('masonry-layout');
+const loaded = require('imagesloaded');
 
-const msnry = new Masonry('.masonry-grid', {
-  itemSelector: '.masonry-item',
-  columnWidth: 250,
-  gutter: 10
+const grid = document.querySelector('.masonry-grid');
+
+loaded(grid, () => {
+  const msnry = new Masonry(grid, {
+    itemSelector: '.masonry-item',
+    columnWidth: 250,
+    gutter: 10
+  });
 });
