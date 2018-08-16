3978dd0e4c8edb25b9fd5425d25301502f80cef6
Hewitt <roy_hewitt@fws.gov>
2018-08-16 12:36:41 -0400

    Update script to create ALL necessary folders to build site

--- build/create-dist.js
+++ build/create-dist.js
@@ -1,7 +1,18 @@
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
-const directories = ['dist/js', 'dist/data', 'dist/css', 'dist/images/hero', 'dist/images/pages'];
+const directories = [
+  'dist/js',
+  'dist/data',
+  'dist/css',
+  'dist/images/hero',
+  'dist/images/pages',
+  'site/static/css',
+  'site/static/data',
+  'site/static/js',
+  'site/static/images/hero',
+  'site/static/images/pages'
+];
 
 // Don't blast away CSS, JavaScript, or Images or we'll have to just re-copy them
 const options = { glob: { ignore: ['dist/css', 'dist/js', 'dist/images', 'dist/data'] } };
