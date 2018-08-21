d4173b5f7fe158a418ba9e1e728f8bc3fd6fdfbc
Roy Hewitt <roy_hewitt@fws.gov>
2018-08-21 11:12:31 -0400

    Implement infinite scroll on paginated content

--- src/js/infinite-scroll/index.js
+++ src/js/infinite-scroll/index.js
@@ -1,25 +1,10 @@
-const throttle = require('lodash.throttle');
+const InfiniteScroll = require('infinite-scroll');
 
-const list = document.querySelector('.infinite-list');
+const elm = document.querySelector('.card-list');
 
-const closest = (elem, selector) => {
-  for ( ; elem && elem !== document; elem = elem.parentNode ) {
-    if ( elem.matches( selector ) ) return elem;
-  }
-  return null;
+const options = {
+  path: '.pagination-next',
+  append: '.card'
 };
 
-const lazyLoadImages = e => {
-  const SRC_ATTRIBUTE = 'data-src';
-  const nearestLazyImg = list.querySelector(`[${SRC_ATTRIBUTE}]`);
-  const nearestLazyItem = closest(nearestLazyImg, '.infinite-list--item');
-  if (!nearestLazyItem) return;
-  const lazyImgFromView = nearestLazyItem.getBoundingClientRect().top - window.innerHeight;
-
-  if (lazyImgFromView < 700){
-    nearestLazyImg.src = nearestLazyImg.getAttribute(SRC_ATTRIBUTE);
-    nearestLazyImg.removeAttribute(SRC_ATTRIBUTE);
-  }
-}
-
-window.addEventListener('scroll', throttle(lazyLoadImages, 75));
+const infScroll = new InfiniteScroll(elm, options);
