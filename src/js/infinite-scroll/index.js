74c305157f9d720fc5efdc0b611a0bd342c63868
Roy Hewitt <roy_hewitt@fws.gov>
2018-08-22 14:42:34 -0400

    Remove pagination once infinite scroll kicks in

--- src/js/infinite-scroll/index.js
+++ src/js/infinite-scroll/index.js
@@ -1,11 +1,16 @@
 const InfiniteScroll = require('infinite-scroll');
 
 const elm = document.querySelector('.card-list');
-const pagination = document.querySelector('.pagination-next');
+const pagination = document.querySelector('.pagination');
+
+const remove = el => el.parentNode.removeChild(el);
 
 const options = {
   path: '.pagination-next',
   append: '.card'
 };
 
-if (pagination) new InfiniteScroll(elm, options);
+if (pagination) {
+  const infScroll = new InfiniteScroll(elm, options);
+  infScroll.once('append', () => remove(pagination));
+}
