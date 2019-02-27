2c81b790005f5eb08fc9b2f95b8443324b16ae17
Roy Hewitt <roy_hewitt@fws.gov>
2019-02-27 08:03:13 -0500

    Update hugo, sharp

--- build/hugo.js
+++ build/hugo.js
@@ -5,15 +5,8 @@ const chalk = require('chalk');
 function build(env) {
   const environment = env || process.argv[2];
   console.log(chalk.yellow(`Environment: ${environment} ${endOfLine}`));
-  const isProduction = environment === 'production';
-  const config = environment === 'production' ? 'site/config.yml' : 'site/config.staging.yml';
-  const command = [
-    'hugo',
-    `--config=${config}`,
-    '--source=site/',
-    `--buildDrafts=${isProduction ? 'false' : 'true'}`,
-    `--buildFuture=${isProduction ? 'false' : 'true'}`
-  ].join(' ');
+  const config =    environment === 'production' ? 'config.yml' : 'config.staging.yml';
+  const command = ['hugo', `--config=${config}`, '--source=site'].join(' ');
 
   exec(command, (err, stdout, stderr) => {
     if (err) console.error(err);
