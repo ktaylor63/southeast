acdc28e1d182bfda6d2ebbfe09434823fb30140a
Roy Hewitt <roy_hewitt@fws.gov>
2019-11-07 07:43:00 -0500

    Update permit page to display multiple results

--- src/js/10a1a-permits/index.js
+++ src/js/10a1a-permits/index.js
@@ -1,7 +1,7 @@
 const xhr = require('xhr');
 
 let hasWWW = window.location.href.indexOf('www');
-hasWWW = (hasWWW < 0) ? false : true;
+hasWWW = !((hasWWW < 0));
 const baseURL = document.body.getAttribute('data-root');
 const dataURL = hasWWW ? baseURL : baseURL.replace('www.', '');
 
@@ -10,24 +10,30 @@ const input = document.getElementById('permit-search');
 let permits;
 
 xhr.get(`${dataURL}/data/permits.js`, (err, res, body) => {
+  if (err) console.log(err);
   permits = JSON.parse(body);
   input.addEventListener('keyup', search);
 });
 
 function search(e) {
-  const query = e.target.value.toLowerCase();
-  const matches = permits.filter(permit => permit.number.toLowerCase() === query);
+  const query = e.target.value;
+  const regex = new RegExp(query, 'gi');
+  const matches = permits.filter(permit => regex.test(permit.number));
+  console.log(matches);
 
   if (query.length === 0) return output.innerHTML = '';
 
-  if (matches.length === 1) output.innerHTML = render(matches[0]);
-  else output.innerHTML = `<p>Could not find a permit application matching that number.</p>`;
+  if (matches) output.innerHTML = render(matches);
+  else output.innerHTML = '<p>Could not find a permit application matching that number.</p>';
 }
 
-
-function render(result) {
+function createListItem(match) {
   return `
-    <p>Status: ${result.status}</p>
-    <p><em>Federal Register</em> Status: ${result.fed_register}</p>
+    <li class="card card-text">
+      <p>Status for ${match.number}: ${match.status}</p>
+      <p><em>Federal Register</em> Status: ${match.fed_register}</p>
+    </li>
   `;
 }
+
+const render = (matches) => `<ul class="card-list">${matches.map(createListItem).join('')}</ul>`;
