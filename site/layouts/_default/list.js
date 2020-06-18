7427b5ade0e0b5b85e7099897e2b52729edb7e15
Roy Hewitt <roy_hewitt@fws.gov>
2020-06-18 16:15:51 -0400

    Guard statement if no hero exists

--- site/layouts/_default/list.js
+++ site/layouts/_default/list.js
@@ -1,6 +1,8 @@
 {{- $.Scratch.Add "section" .Section -}}
 {{- $.Scratch.Add "index" slice -}}
 {{- range where .Site.RegularPages "Section" ($.Scratch.Get "section") -}}
+{{- if .Params.hero -}}
 {{- $.Scratch.Add "index" (dict "uri" .Permalink "title" .Title "summary" .Summary "tags" .Params.tags "img" .Params.hero.name "alt" .Params.hero.alt "caption" .Params.hero.caption "date" .Params.date "updated" .Params.updated "lat" .Params.lat "lng" .Params.lng) -}}
+{{- end -}}
 {{- end -}}
 {{- $.Scratch.Get "index" | jsonify -}}
